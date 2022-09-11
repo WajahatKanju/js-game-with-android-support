b2e0e0ed318f5b9e4156d20ab4a937beff5a0b49
WjahatAhmad <wajahat5ahmad@gmail.com>
2022-09-11 18:21:46 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener('load', () => {
+  const canvas = document.getElementById('canvas');
+  const ctx=  canvas.getContext('2d');
+
+  class inputHandler{}
+
+  class Player{
+
+  }
+  class Enemy{
+
+  }
+  class Background{
+
+  }
+
+  function handleEnemies(){}
+
+  function displayStatus(){
+
+  }
+
+  function animate(){
+
+  }
+
+
+})
